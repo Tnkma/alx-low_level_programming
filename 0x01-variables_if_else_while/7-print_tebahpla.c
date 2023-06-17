4ca0616ff81f89359d1881878e0f09f5d68f4897
Tnkma <onwusilikegodswill@gmail.com>
2023-06-17 15:20:34 -0700

    prints the alphaeb in reverse form

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = 'z'; m >= 'a'; m--)
+		putchar(m);
+	putchar('\n');
+
+	return (0);
+}
