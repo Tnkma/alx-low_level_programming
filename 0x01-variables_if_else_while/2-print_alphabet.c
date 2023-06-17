8c1defba8450726d1acad494448f42e1eefb7c20
Tnkma <onwusilikegodswill@gmail.com>
2023-06-17 04:13:44 -0700

    prints the alphabet in low character

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	/**integer n is going to hold the alphabets while we loop through**/
+	for (n = 'a'; n <= 'z'; n++)
+
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
