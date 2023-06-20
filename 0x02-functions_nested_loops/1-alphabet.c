1048cbb10fcb934867e745d5f0fbdad2384a5a26
Tnkma <onwusilikegodswill@gmail.com>
2023-06-20 14:32:54 -0700

    prints the alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lower 
+ *
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
+	_putchar('\n');
+}
