5fa77ec47eee9d6a7b93667d80082ad7823cac2d
Tnkma <onwusilikegodswill@gmail.com>
2023-06-21 01:20:45 -0700

    checks for lower character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int _islower(int c)
+{
+	if (islower(c) != 0)
+		return (1);
+	else
+		return (0);
+}
