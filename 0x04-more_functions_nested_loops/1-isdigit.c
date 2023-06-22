8673347f9c18a431312e0b047a08dd62e3ab3ef2
Tnkma <onwusilikeonso@gmail.com>
2023-06-22 05:11:09 -0700

    checks for digits in the code

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for numbers
+ * @c: inputed value to be checked
+ *
+ *
+ * Return: Always 0 (success)
+ */
+
+int _isdigit(int c)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (c == num)
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 int _isupper(int c);
+int _isdigit(int c);
 
 
 
