2b2e342668218b96da1b89a878013751e981c87b
Tnkma <onwusilikeonso@gmail.com>
2023-06-26 11:57:07 -0700

    counts the strng and return the count

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - the function to count the string
+ * @s: the string to be counted
+ *
+ * Return: Always void
+ */
+
+int _strlen(char *s)
+{
+	/* A variable to count the string */
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+
+	}
+	return (c);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 void reset_to_98(int *n);
 #include <stdio.h>
 void swap_int(int *a, int *b);
+#include <string.h>
+int _strlen(char *s);
 
 #endif
