9840b716d2dbaf49be1cffe647072b6ec79bcd83
Tnkma <onwusilikeonso@gmail.com>
2023-07-03 15:18:08 -0700

    memset prototype

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,14 +11,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	unsigned int j = 0;
-
-	while (j < n && b != '\0')
-	{
-		for (i = 0; i < n; i++)
-			j++;
-	}
+	memset(s, b, n);
 	return (s);
 }
 
