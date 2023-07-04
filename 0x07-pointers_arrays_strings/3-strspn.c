0e4fbe38deaf6888a32939f114aae8eff9b56c90
Tnkma <onwusilikeonso@gmail.com>
2023-07-04 12:59:31 -0700

    strspn prototype

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strspn - returns the number of bits in the initial segment
+ * @s: the memory to compare to
+ * @accept: the character to check
+ *
+ * Return: a pointer with the count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int m;
+
+	m = strspn(s,accept);
+
+	return (m);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,7 @@ char *_memset(char *s, char b, unsigned int n);
 #include <stdlib.h>
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+
 
 #endif
