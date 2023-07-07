88fb0960e3da8e699a522f14a356934855dc2b70
Tnkma <134863771+Tnkma@users.noreply.github.com>
2023-07-07 22:29:51 +0100

    Update 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - print the excalt number
+ *@n: checks the number
+ *
+ * Return: Always 0 (success)
+ */
+
+int _abs(int n)
+{
+	int num;
+
+	num = abs(n);
+
+	return (num);
+}
