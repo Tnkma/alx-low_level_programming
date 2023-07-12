e996fa114d77029abf21271f58b1e20f205aa3cd
Tnkma <onwusilikeonso@gmail.com>
2023-07-12 13:07:35 -0700

    sorted betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *ptr = (char*)malloc(size*sizeof(char));
+	char *ptr = (char *)malloc(size * sizeof(char));
 
 
 	if (ptr == NULL)
