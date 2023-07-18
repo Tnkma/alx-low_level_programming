d1778d8f36de3878d85ec3e3dd8ebab09a839157
Tnkma <onwusilikeonso@gmail.com>
2023-07-18 15:36:37 -0700

    created a dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+/**
+ * struct dog - short description
+ * @name: first memeber
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Longer ones
+ */
+
 struct dog
 {
 	char *name;
