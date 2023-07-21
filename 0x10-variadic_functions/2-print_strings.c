9d59dda1982399a2e599d7dd8d53ed8b15ba6786
Tnkma <onwusilikeonso@gmail.com>
2023-07-21 05:44:47 -0700

    prints the string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints the strings
+ * @separator: separtes the strings
+ * @n: number of the strings
+ *
+ * Return: Always void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *show;
+
+	va_list print_em;
+
+	va_start(print_em, n);
+
+	for (i = 0; i < n; i++)
+	{
+		show = va_arg(print_em, char*);
+
+		if (show == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", show);
+		}
+
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
