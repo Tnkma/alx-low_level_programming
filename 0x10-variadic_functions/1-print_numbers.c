09f031f620adca5be111f4df727da1f697e0263f
Tnkma <onwusilikeonso@gmail.com>
2023-07-21 05:03:01 -0700

    print the number with comma

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the number with spaces
+ * @separator: the string that sets the separation
+ * @n: the inputed number
+ *
+ * Return: a void pointer
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int show;
+	unsigned int i;
+
+	va_list print_em;
+
+	va_start(print_em, n);
+
+	for (i = 0; i < n; i++)
+	{
+		show = va_arg(print_em, int);
+		printf("%d", show);
+
+		if (i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(print_em);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 int sum_them_all(const unsigned int n, ...);
 #include <stdio.h>
 #include <stdarg.h>
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
