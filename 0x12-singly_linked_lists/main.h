8f93717f46c4bbe0f2a9c8bc74da5de7098eda6d
Tnkma <onwusilikeonso@gmail.com>
2023-07-27 20:43:42 -0700

    prints all the elements of the list

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -16,6 +16,9 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 size_t list_len(const list_t *h);
