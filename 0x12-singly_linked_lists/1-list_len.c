7e25b2ec187128c30267ca59b826fc13ac63a691
Tnkma <onwusilikeonso@gmail.com>
2023-07-29 01:31:31 -0700

    fixed betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * list_len - program to prints the number of elements found in a list
- * @list_t: holds the nodes
+ * list_len - prints the number of elements
  * @h: points to the head
  *
- * return: Always 0 for success
+ * Return: Always 0 for success
  */
 
 size_t list_len(const list_t *h)
 {
+
 	size_t m = 0;
 
 	while (h)
