baab3d671b873fe860377bd07bf20a82454034c3
Tnkma <onwusilikeonso@gmail.com>
2023-07-29 06:50:35 -0700

    returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,31 +1,51 @@
 #include "main.h"
 
-/**
- * is_palindrome - checks if a string is pallidrome or not
- * @s: the string to check
- *
- *
- * Return: either 1 or 0
- */
+int is_palindrome_helper(char *start, char *end);
 
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: The string to check
+ *
+ * Return: 1 if s is a palindrome, and 0 otherwise
+ */
 int is_palindrome(char *s)
 {
-	int m;
-	int com = 0;
-	int len;
 
-	len = strlen(s);
-	for (m = 0; m < len / 2; m++)
-	{
-		if (s[m] == s[len - m - 1])
-			com++;
-	}
-	if (com == m)
+	int len = 0;
+
+
+	if (*s == '\0')
 	{
 		return (1);
 	}
-	else
+
+	for (len = 0; s[len] != '\0'; len++)
+
+		;
+
+	return (is_palindrome_helper(s, s + len - 1));
+
+}
+
+/**
+ * is_palindrome_helper - Recursively checks if a string is a palindrome
+ * @start: Pointer to the start of the substring to check
+ * @end: Pointer to the end of the substring to check
+ *
+ * Return: 1 if the substring is a palindrome, and 0 otherwise
+ */
+int is_palindrome_helper(char *start, char *end)
+{
+
+	if (start >= end)
+	{
+		return (1);
+	}
+	else if (*start != *end)
 	{
 		return (0);
 	}
+	else
+		return (is_palindrome_helper(start + 1, end - 1));
 }
+
