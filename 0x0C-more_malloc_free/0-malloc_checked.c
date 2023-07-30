2e1fd20e280da7bb0e768470100e4d9fc105a652
Tnkma <onwusilikeonso@gmail.com>
2023-07-29 17:00:58 -0700

    allocates more memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the varibale to allocate memory to
+ *
+ * Return: A pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+
+	return (ptr);
+}
