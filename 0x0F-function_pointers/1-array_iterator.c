bf8cb07875a44acef8847db86fde076cb9750611
Tnkma <onwusilikeonso@gmail.com>
2023-07-31 08:03:39 -0700

    iterates through an input

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through the array
+ * @array: The array of integers
+ * @size: size of the integers
+ * @action: actions performed
+ *
+ * Return: Non since were void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int m = 0;
+
+
+	if (array && action)
+	{
+		while (m < size)
+		{
+			action(array[m]);
+			m++;
+		}
+	}
+}
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 
 
