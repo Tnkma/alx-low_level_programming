a8b845e82453655e5ea217067e230e672e59dbf3
Tnkma <onwusilikeonso@gmail.com>
2023-07-31 03:31:23 -0700

    checked for NULL input

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+
 	strcpy(d->name, name);
 	d->age = age;
 	strcpy(d->owner, owner);
+
 }
