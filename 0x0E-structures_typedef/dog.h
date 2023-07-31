2de0c5bacddf68c2d4298b2b8976ac73a934b0de
Tnkma <onwusilikeonso@gmail.com>
2023-07-31 04:23:40 -0700

    defined a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,13 @@
  * Description: Longer ones
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
+dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #include <string.h>
