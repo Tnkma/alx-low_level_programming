aeb8e9debe8dcbaab30766e5e5c1ae995a9565de
Tnkma <onwusilikeonso@gmail.com>
2023-07-31 07:31:35 -0700

    compares two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * wildcmp - the function thats checks for equality
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: Always 0 for success
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) != '\0' && *s1 == '\0')
+		{
+			return (0);
+		}
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}
+
