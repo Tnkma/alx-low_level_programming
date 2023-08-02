a063bb7c8da0255394b3fa94dbc7514aa5f6ce4e
Tnkma <onwusilikeonso@gmail.com>
2023-08-02 00:26:00 -0700

    a 64bit program in assambly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,17 @@
-; hello.asm
 section .data
-    message db 'Hello, Holberton', 10, 0 ; The message to print, 10 = '\n', 0 = NULL
+	fmt db	"%s", 10, 0
+	msg db	"Hello, Holberton", 0
 
-section .text
-    extern printf
-    global main
+	section .text
+	extern printf
+	global main
 
 main:
-    ; Prepare the arguments for printf
-    mov rdi, message   ; First argument is the format string
-    xor rax, rax       ; Clear rax to signify there are no vector registers used
-
-    ; Call printf
-    call printf
-
-    ; Return from main
-    mov eax, 60    ; syscall number for exit is 60
-    xor edi, edi   ; return code 0
-    syscall        ; make syscall
-
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
