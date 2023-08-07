ab038173b1562350bddae05195006f0bbf03108e
Tnkma <onwusilikeonso@gmail.com>
2023-08-07 15:46:12 -0700

    reads and prints a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads and prints out the txt file
+ * @filename: the name of the file
+ * @letters: returns the count of the txt
+ *
+ * Return: the counts of the txt
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	size_t count;
+	ssize_t	now_return;
+	char *buf;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		return (0);
+	}
+	buf = malloc(letters + 11);
+	if (buf == NULL)
+	{
+		fclose(fp);
+		return (0);
+	}
+
+	count = fread(buf, sizeof(char), letters, fp);
+	if (ferror(fp))
+	{
+		free(buf);
+		fclose(fp);
+		return (0);
+	}
+	buf[count] = '\0';
+	now_return = fwrite(buf, sizeof(char), count, stdout);
+	if (now_return < -1)
+	{
+		free(buf);
+		fclose(fp);
+		return (0); }
+
+	free(buf);
+	fclose(fp);
+
+	return (now_return);
+}
