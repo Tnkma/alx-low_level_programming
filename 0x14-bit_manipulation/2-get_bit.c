0f117e2dd55d0599b5966244a62e55223cc4c259
Tnkma <onwusilikegodswill@gmail.com>
2023-08-31 13:11:55 -0700

    gets the value of bit at an index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_bit - get the value of bit at agiven index
+ * @n: the number
+ * @index: the given index
+ *
+ * Return: the value at a given index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int mask;
+	int value;
+	
+	mask = 1 << index;
+
+	if (index > 32)
+	{
+		return (-1);
+	}
+	else
+	{
+		value = (n & mask) >> index;
+	}
+	return (value);
+}
