7bf1d1dc54ac6224f4051b840a0a8d17086d4984
Tnkma <onwusilikegodswill@gamil.com>
2023-10-12 07:19:41 -0700

    print the number

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - length of the elements in a node
+ * @: the node list
+ *
+ * Return: the count of the node
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
