545a7445a2cb199bb9c01978b1230ccac408486f
Tnkma <onwusilikegodswill@gmail.com>
2023-11-03 19:57:09 +0100

    created a hash table that allocates memory for the linked list

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/** assign the new list the size of the array */
 	new_hash_table->size = size;
 	/** allocate memory for the array to point to the next list */
-	new_hash_table->array = malloc(sizeof(hash_table_t) * size);
+	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (!new_hash_table->array)
 	{
 		free(new_hash_table);
