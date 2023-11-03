c9701ccd535125141c4c28afd052c892f532cfeb
Tnkma <onwusilikegodswill@gmail.com>
2023-11-03 19:45:23 +0100

    created a hash table that allocates memory for the linked list

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates the hash function
+ * @size: the size of the array
+ *
+ * Return: the created function
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table = NULL;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+	/** check if memory allocation is successful */
+	if (!new_hash_table)
+	{
+		return (NULL);
+	}
+	/** assign the new list the size of the array */
+	new_hash_table->size = size;
+	/** allocate memory for the array to point to the next list */
+	new_hash_table->array = malloc(sizeof(hash_table_t) * size);
+	if (!new_hash_table->array)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+
+	/** we will have to return a  pointer to the created new_hash_table */
+	return (new_hash_table);
+}
