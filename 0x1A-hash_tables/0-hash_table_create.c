1a5f13b841a1c98396658f516a7c00a40c5bffb9
Tnkma <onwusilikegodswill@gmail.com>
2023-11-03 19:48:51 +0100

    created a hash table that allocates memory for the linked list

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash_table = NULL;
+	hash_table_t *new_hash_table;
 
 	new_hash_table = malloc(sizeof(hash_table_t));
 	/** check if memory allocation is successful */
