153a5f141807b96bfb59e7e0fa6d6094b3c7e13a
Tnkma <onwusilikegodswill@gmail.com>
2023-12-22 12:45:09 +0100

    perimeter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A program that returns the perimeter of the island 
+"""
+def island_perimeter(grid):
+    """
+    0 is water while 1 is land
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edge = 0
+    size = 0
+
+    for row in range(height):
+        for col in range(width):
+            if grid[row][col] == 1:
+                size += 1
+                if (col > 0 and grid[row][col - 1] == 1):
+                    edge += 1
+                if (row > 0 and grid[row - 1][col] == 1):
+                    edge += 1
+
+    return size * 4 - edge * 2
