6b0fa964c8be2b74a272cdda595f725fc5bf6b97
Tnkma <onwusilikegodswill@gmail.com>
2024-02-07 17:17:39 +0100

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,8 +5,22 @@
  * @size: the size of the array
  * @value: the value to find
  * Return: the value gotten or -1 if NULL
-*/
+ */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (array[i]);
+		}
+	}
+	return (-1);
 }
